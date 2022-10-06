9ee3b4a6e2de95d099114510c185617b5ab841f7
Han Ming Yan <mingyan.han.2021@scis.smu.edu.sg>
2022-10-06 20:30:05 +0800

    added extra imports to the code in ingredients

---
+++ recipeapp_backend/src/main/java/org/forksmash/recipeapp_backend/userprofile/User.java
@@ -0,0 +1,95 @@
+package org.forksmash.recipeapp_backend.userprofile;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+/* Implementations of UserDetails to provide user information to Spring Security, 
+e.g., what authorities (roles) are granted to the user and whether the account is enabled or not
+*/
+public class User implements UserDetails{
+    private static final long serialVersionUID = 1L;
+
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+    
+    @NotNull(message = "Username should not be null")
+    @Size(min = 5, max = 20, message = "Username should be between 5 and 20 characters")
+    private String username;
+    
+    @NotNull(message = "Password should not be null")
+    @Size(min = 8, message = "Password should be at least 8 characters")
+    private String password;
+
+    @NotNull(message = "Authorities should not be null")
+    // We define two roles/authorities: ROLE_USER or ROLE_ADMIN
+    private String authorities;
+
+    @ManyToMany
+    @JoinTable(name = "user_ingredient", joinColumns = @JoinColumn(name = "quantity"),
+     joinColumns = @JoinColumn(name = "ingredientID"), 
+     inverseJoinColumns = @JoinColumn(name = "userID"))
+
+    public User(String username, String password, String authorities){
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    
+
+
+    /* Return a collection of authorities granted to the user.
+    */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority(authorities));
+    }
+
+    /*
+    The various is___Expired() methods return a boolean to indicate whether
+    or not the user’s account is enabled or expired.
+    */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+        public boolean isAccountNonLocked() {
+    return true;
+    }
+    @Override
+        public boolean isCredentialsNonExpired() {
+    return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
