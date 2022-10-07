3177f9deafeefa5501d0617ae9dc536c5f8a3670
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-10-07 11:02:48 +0800

    RegistrationService work in progress

--- recipeapp_backend/src/main/java/org/forksmash/recipeapp_backend/registration/RegistrationService.java
+++ recipeapp_backend/src/main/java/org/forksmash/recipeapp_backend/registration/RegistrationService.java
@@ -1,6 +1,7 @@
 package org.forksmash.recipeapp_backend.registration;
 
 import org.forksmash.recipeapp_backend.appuser.AppUser;
+import org.forksmash.recipeapp_backend.appuser.AppUserRole;
 import org.forksmash.recipeapp_backend.appuser.AppUserService;
 import org.springframework.stereotype.Service;
 import lombok.*;
@@ -14,6 +15,14 @@ public class RegistrationService {
         boolean isValidEmail = emailValidator.test(request.getEmail());
         if (!isValidEmail)
             throw new IllegalStateException("Email " + request.getEmail() + " is not valid");
-        return appUserService.signUpUser(new AppUser(request.getFirstName(), request.getLastName(), request.getEmail(), request.getPassword()));
+        return appUserService.signUpUser(
+            new AppUser(
+                request.getFirstName(), 
+                request.getLastName(), 
+                request.getEmail(), 
+                request.getPassword(),
+                AppUserRole.USER
+            )
+        );
     }
 }
