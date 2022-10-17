22f8e7a4ba9578de6bd36973f84440dc31076dc8
Haiyang Xu <haiyang.xu.2021@scis.smu.edu.sg>
2022-10-17 20:47:59 +0800

    Insert into ingredient TABLE (SQL code)

--- database-setup.sql
+++ database-setup.sql
@@ -54,3 +54,51 @@ CREATE TABLE favourite_recipes(
     constraint favourite_recipes_user_fk foreign key(userid) references users(userid),
     constraint favourite_recipes_recipe_fk foreign key(recipeid) references recipe(recipeid)
 );
+
+/*INSERT INTO ingredient TABLE*/
+INSERT INTO ingredient VALUES
+(101, "meats", "Beef"),
+(102, "meats", "Chicken"),
+(103, "meats", "Duck"),
+(104, "meats", "Pork"),
+(105, "meats", "Lamb"),
+(106, "meats", "Venison"),
+
+(201, "seafoods", "Salmon"),
+(202, "seafoods", "Tuna"),
+(203, "seafoods", "Mackerel"),
+(204, "seafoods", "Cod"),
+(205, "seafoods", "Squid"),
+(206, "seafoods", "Prawn"),
+
+(301, "vegetables", "Asparagus"),
+(302, "vegetables", "Bean"),
+(303, "vegetables", "Broccoli"),
+(304, "vegetables", "Cauliflower"),
+(305, "vegetables", "Cabbage"),
+(306, "vegetables", "Egg Plant"),
+(307, "vegetables", "Onion"),
+(308, "vegetables", "Potato"),
+(309, "vegetables", "Tomato"),
+(310, "vegetables", "Kimchi"),
+
+(401, "dairies", "Butter"),
+(402, "dairies", "Cheese"),
+(403, "dairies", "Cream"),
+(404, "dairies", "Milk"),
+(405, "dairies", "Yoghurt"),
+
+(501, "fruits", "Apple"),
+(502, "fruits", "Avocado"),
+(503, "fruits", "Banana"),
+(504, "fruits", "Blackberry"),
+(505, "fruits", "Blueberry"),
+(506, "fruits", "Grapefruit"),
+(507, "fruits", "Orange"),
+(508, "fruits", "Pineapple"),
+(509, "fruits", "Lemon"),
+(510, "fruits", "Lime"),
+(511, "fruits", "Strawberry"),
+
+(601, "others", "Miso"),
+(602, "others", "Egg");
