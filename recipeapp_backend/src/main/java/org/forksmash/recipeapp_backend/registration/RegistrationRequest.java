2c08d8d768890ff85fbde5394041cf20f1a5e20b
Han Ming Yan <mingyan.han.2021@scis.smu.edu.sg>
2022-10-23 21:41:29 +0800

    added registration folder

---
+++ recipeapp_backend/src/main/java/org/forksmash/recipeapp_backend/registration/RegistrationRequest.java
@@ -0,0 +1,18 @@
+package org.forksmash.recipeapp_backend.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+    //add in other credentials here later on
+}
