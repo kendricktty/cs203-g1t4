e4bbd4494202731ee78f8919cc1132df00dcd521
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-11-06 21:25:21 +0800

    Fixed NutrientService by ignoring nutrients that are not in MEAL_BREAKDOWN map

--- recipeapp_backend/src/main/java/org/forksmash/recipeapp_backend/nutrient/NutrientProcessingException.java
+++ recipeapp_backend/src/main/java/org/forksmash/recipeapp_backend/nutrient/NutrientProcessingException.java
@@ -3,7 +3,7 @@ package org.forksmash.recipeapp_backend.nutrient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class NutrientProcessingException extends RuntimeException {
 
     public NutrientProcessingException(Throwable exception) {
